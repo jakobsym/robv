e5eaf97028e67657843a4245ab4458dea5b8f675
jakobsym <jakob.symchych@my.utsa.edu>
2024-10-13 17:55:04 -0500

    fixed bugs when fetching token address from args

--- src/lib/cl.js
+++ src/lib/cl.js
@@ -1,24 +1,26 @@
-import { error } from "console";
+import { PublicKey } from "@solana/web3.js";
 import { showGreeting  } from "../utils/greeting.js";
 import { baseMintAddressFromArgs, quoteMintFromArgs } from "../utils/helper.js"
 import { getOrderBookData, getOrderBookMarketAddress } from "./serum_scripts.js";
 
 export const runCLI = async() =>  {
     var pairMap = {}
+    var baseMintID, quoteMintID
     try {
         await showGreeting()
-        pairMap = await fetchPairAddress()
-        //let obMarketAddress = await getOrderBookMarketAddress(pairMap['baseMintAddress'], pairMap['quoteMintAddress'])
+        quoteMintID = await quoteMintFromArgs()
+        baseMintID = await baseMintAddressFromArgs()
+        let obMarketAddress = await getOrderBookMarketAddress(baseMintID, quoteMintID)
+        console.log(obMarketAddress)
         //let obData = await getOrderBookData(obMarketAddress)
     } catch (error) {
         console.error(error)
     }
 }
 
-// returns pair as hashmap for quick lookup
+// returns pair as PublicKeys in a hashmap for quick lookup
 const fetchPairAddress = async() => {
     var baseMintAddress, quoteMintAddress
-
     try {
         quoteMintAddress = await quoteMintFromArgs()
         baseMintAddress = await baseMintAddressFromArgs()
@@ -30,7 +32,7 @@ const fetchPairAddress = async() => {
         console.error("error fetching addresses from CLI")
     }
     return {
-        BaseMintAddress: baseMintAddress,
-        QuoteMintAddress: quoteMintAddress,
+        BaseMintAddress: new PublicKey(baseMintAddress),
+        QuoteMintAddress: new PublicKey(quoteMintAddress),
     }
 }
--- src/lib/serum_scripts.js
+++ src/lib/serum_scripts.js
@@ -1,3 +1,5 @@
+import { Connection, PublicKey } from '@solana/web3.js'
+import { Market } from '@project-serum/serum'
 // uses project-serum DEX to fetch orderbook data (bids/asks + size)
 // TODO: How will end user see data so its meaningful?
 
@@ -7,6 +9,28 @@ export const getOrderBookData = async(marketAddress) => {
 }
 
 export const getOrderBookMarketAddress = async(baseMintAddress, quoteMintAddress) => {
+    const connection = new Connection('https://api.mainnet-beta.solana.com', 'confirmed');
+    
+    const SERUM_PROGRAM_ID = new PublicKey('9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin');
+    const BASE_ID = new PublicKey(baseMintAddress)
+    const QUOTE_ID = new PublicKey(quoteMintAddress)
 
+    const markets = await Market.findAccountsByMints(
+        connection,
+        BASE_ID,
+        QUOTE_ID,
+        SERUM_PROGRAM_ID
+    );
+
+    if (markets.length === 0) {
+        console.log('No market found for the given token pair');
+        return null;
+    }
+
+    console.log(`Found ${markets.length} market(s):`);
+    markets.forEach((market, index) => {
+        console.log(`${index + 1}. Market address: ${market.publicKey.toBase58()}`);
+    });
+
+    return markets[0].publicKey;
 }
-
